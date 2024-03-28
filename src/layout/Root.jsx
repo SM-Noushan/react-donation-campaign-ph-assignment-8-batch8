82d1399258f2f5d4d3ba0d4c54405806b9e82e6f
SMNoushan <noushan.md@gmail.com>
2024-03-28 23:29:50 +0600

    added navbar

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import { MyNavbar } from "../pages/shared/navbar/MyNavbar";
 
 const Root = () => {
     return (
         <div>
+            <MyNavbar />
             <Outlet />
         </div>
     );
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,10 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../layout/Root";
+import Error from "../pages/error/Error";
+import Home from "../pages/home/Home";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Root />,
+        errorElement: <Error />,
+        children: [
+            {
+                index: true,
+                element: <Home />
+            }
+        ]
     },
 ]);
 
